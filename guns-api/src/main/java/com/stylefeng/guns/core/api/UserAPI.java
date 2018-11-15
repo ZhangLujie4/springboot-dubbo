95c39485249962037c3218150a1fc0746dc91640
zhanglujie <you@example.com>
2018-11-15 14:41:46 +0800

    项目初始化

---
+++ guns-api/src/main/java/com/stylefeng/guns/core/api/UserAPI.java
@@ -0,0 +1,12 @@
+package com.stylefeng.guns.core.api;
+
+/**
+ * @author Tori
+ * @version V1.0.0
+ * @date 2018-11-15 13:59
+ * @description
+ */
+public interface UserAPI {
+
+    boolean login(String userName, String password);
+}
