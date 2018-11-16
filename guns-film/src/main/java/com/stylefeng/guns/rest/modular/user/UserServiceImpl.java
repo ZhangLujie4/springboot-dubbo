c72a7e604fc16cac5796b01b8eb94bbdc896b6db
zhanglujie <you@example.com>
2018-11-16 16:13:50 +0800

    初创film模块

---
+++ guns-film/src/main/java/com/stylefeng/guns/rest/modular/user/UserServiceImpl.java
@@ -0,0 +1,142 @@
+package com.stylefeng.guns.rest.modular.user;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.stylefeng.guns.core.api.UserAPI;
+import com.stylefeng.guns.core.api.UserInfoModel;
+import com.stylefeng.guns.core.api.UserModel;
+import com.stylefeng.guns.core.util.MD5Util;
+import com.stylefeng.guns.rest.common.persistence.dao.MoocUserTMapper;
+import com.stylefeng.guns.rest.common.persistence.model.MoocUserT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import sun.security.provider.MD5;
+
+import java.util.Date;
+
+/**
+ * @author Tori
+ * @version V1.0.0
+ * @date 2018-11-15 13:59
+ * @description
+ */
+
+// 配置轮询的负载均衡策略
+@Component
+@Service(interfaceClass = UserAPI.class, loadbalance = "roundrobin")
+public class UserServiceImpl implements UserAPI {
+
+    @Autowired
+    private MoocUserTMapper moocUserTMapper;
+
+    @Override
+    public int login(String userName, String password) {
+
+        // 根据登录账号获取数据库信息
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userName);
+
+        MoocUserT result = moocUserTMapper.selectOne(moocUserT);
+
+        // 获取到的结果,然后返回userId
+        if (result != null && result.getUuid() > 0) {
+            String md5Password = MD5Util.encrypt(password);
+            if (result.getUserPwd().equals(md5Password)) {
+                return result.getUuid();
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean register(UserModel userModel) {
+
+        // 将注册信息实体转换为数据实体
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUserName(userModel.getUsername());
+        moocUserT.setEmail(userModel.getEmail());
+        moocUserT.setAddress(userModel.getAddress());
+        moocUserT.setUserPhone(userModel.getPhone());
+
+        // 创建时间和修改时间 -> current_timestamp
+
+        // 数据加密 【MD5混淆加密 + salt -> shiro加密】
+        String md5Password = MD5Util.encrypt(userModel.getPassword());
+        moocUserT.setUserPwd(md5Password); // 注意
+
+        // 将数据存入数据库
+        Integer insert = moocUserTMapper.insert(moocUserT);
+        if (insert > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean checkUsername(String username) {
+
+        EntityWrapper<MoocUserT> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("user_name", username);
+        Integer result = moocUserTMapper.selectCount(entityWrapper);
+        if (result != null && result > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public UserInfoModel getUserInfo(int uuid) {
+
+        MoocUserT moocUserT = moocUserTMapper.selectById(uuid);
+        UserInfoModel userInfoModel = do2UserInfo(moocUserT);
+
+        return userInfoModel;
+    }
+
+    @Override
+    public UserInfoModel updateUserInfo(UserInfoModel userInfoModel) {
+
+        MoocUserT moocUserT = new MoocUserT();
+        moocUserT.setUuid(userInfoModel.getUuid());
+        moocUserT.setUserSex(userInfoModel.getSex());
+        moocUserT.setUpdateTime(null);
+        moocUserT.setNickName(userInfoModel.getNickname());
+        moocUserT.setBeginTime(null);
+        moocUserT.setLifeState(userInfoModel.getLifeState());
+        moocUserT.setHeadUrl(userInfoModel.getHeadAddress());
+        moocUserT.setBirthday(userInfoModel.getBirthday());
+        moocUserT.setBiography(userInfoModel.getBiography());
+        moocUserT.setEmail(userInfoModel.getEmail());
+        moocUserT.setAddress(userInfoModel.getAddress());
+        moocUserT.setUserPhone(userInfoModel.getPhone());
+
+        // 将数据存入数据库
+        Integer isSuccess = moocUserTMapper.updateById(moocUserT);
+        if (isSuccess>0) {
+            return getUserInfo(moocUserT.getUuid());
+        } else {
+            return userInfoModel;
+        }
+    }
+
+    private UserInfoModel do2UserInfo(MoocUserT moocUserT) {
+        UserInfoModel userInfoModel = new UserInfoModel();
+
+        userInfoModel.setUuid(moocUserT.getUuid());
+        userInfoModel.setUsername(moocUserT.getUserName());
+        userInfoModel.setUpdateTime(moocUserT.getUpdateTime().getTime());
+        userInfoModel.setSex(moocUserT.getUserSex());
+        userInfoModel.setPhone(moocUserT.getUserPhone());
+        userInfoModel.setNickname(moocUserT.getNickName());
+        userInfoModel.setLifeState(moocUserT.getLifeState());
+        userInfoModel.setHeadAddress(moocUserT.getHeadUrl());
+        userInfoModel.setEmail(moocUserT.getEmail());
+        userInfoModel.setCreateTime(moocUserT.getBeginTime().getTime());
+        userInfoModel.setBirthday(moocUserT.getBirthday());
+        userInfoModel.setBiography(moocUserT.getBiography());
+        userInfoModel.setAddress(moocUserT.getAddress());
+        return userInfoModel;
+    }
+}
---
+++ guns-gateway/src/main/java/com/stylefeng/guns/rest/modular/film/FilmController.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.rest.modular.film;
+
+/**
+ * @author Tori
+ * @version V1.0.0
+ * @date 2018-11-16 16:13
+ * @description
+ */
+public class FilmController {
+}
